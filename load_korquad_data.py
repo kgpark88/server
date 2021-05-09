8cd84c447eb378484dcf31832d08a3f907cdd25d
Danny Park <kgpark88@gmail.com>
2021-05-09 21:55:46 +0900

    Add files via upload

--- load_korquad_data.py
+++ load_korquad_data.py
@@ -2,35 +2,33 @@ import os
 import json
 import django
 
-from mrc.models import Passage, Question
+from mrc.models import Sentence, Question
 
 django.setup()
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 # https://github.com/korquad/korquad.github.io/tree/master/dataset
-# json_file = 'KorQuAD_v1.0/KorQuAD_v1.0_dev.json'
 json_file = 'KorQuAD_v1.0_train.json'
-source = "KorQuAD_v1.0"
 
-json_data = json.load(open(json_file, "rt", encoding="UTF8"))
-version = json_data["version"]
-data = json_data["data"]
+json_data = json.load(open(json_file, 'rt', encoding='utf-8'))
+version = json_data['version']
+data = json_data['data']
 for d in data:
     paragraphs = d['paragraphs']
     for p in paragraphs:
-        context = p["context"]
+        context = p['context']
         qas = p['qas']
-        p_text = Passage(passage=context, source=source)
-        p_text.save()
-        print('\n ID {} : CONTEXT : {}'.format(p_text.id, context))
         for q in qas:
-            qa_id = q['id']
+            qid = q['id']
+            sid = qid.split('-')[0]
             question = q['question']
             answers = q['answers']
-            text = answers[0]['text']
-            answer_start = answers[0]['answer_start']
-            defaults = {"passage_id":p_text.id, "question":question, "answer": text, "s_pos": answer_start}
-            print(defaults)
-            Question.objects.update_or_create(no=qa_id, defaults=defaults)
+            answer = answers[0]['text']
+            s_pos = answers[0]['answer_start']
+            defaults = {'sid':sid, 'qid':qid, 'question': question, 'answer': answer, 's_pos':s_pos}
+            Question.objects.update_or_create(qid=qid, defaults=defaults)
+        print(f'{sid}')        
+        Sentence.objects.update_or_create(sid=sid, defaults={'context':context})
+
 
 
